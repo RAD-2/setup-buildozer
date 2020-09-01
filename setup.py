03b28ce2d13a633bc35389994879d8e4693eba42
RAD-2 <70523475+RAD-2@users.noreply.github.com>
2020-09-01 18:16:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from N4Tools import System,Design
 
 commands = [
     'sudo apt-get install python3-setuptools',
-    'sudo apt install git',
+    'sudo apt install git',o
     'git clone https://github.com/kivy/buildozer.git',
     'cd buildozer',
     'sudo python3 setup.py install',
     'sudo apt update',
-    'sudo apt install -y git zip openjdk-8-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev',
-    'pip3 install --user --upgrade cython virtualenv',
+    'sudo apt install -y git zip openjdk-8-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5ade cython virtualenv',
     'sudo apt-get install cython',
     'python3 -m pip install cython',
 ]
